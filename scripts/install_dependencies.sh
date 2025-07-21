9f2c68f2e5e9688a2bf92837a4ae0c73d23f14d4
Ubuntu <ubuntu@ip-172-31-80-241.ec2.internal>
2025-07-21 04:00:56 +0000

    cambios

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,8 +11,14 @@ echo "==> Refrescando paquetes y asegurando dependencias del sistema"
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq python3-venv python3-dev build-essential
 
-echo "==> Instalando dependencias del proyecto"
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+echo "==> Creando entorno virtual"
+python3 -m venv venv
+
+echo "==> Activando entorno virtual"
+source venv/bin/activate
+
+echo "==> Instalando dependencias del proyecto en entorno virtual"
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "==> Instalación completada exitosamente"
