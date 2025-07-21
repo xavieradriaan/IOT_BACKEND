57e0080368b6fd97f0ddafad9430727c31c3d75e
Ubuntu <ubuntu@ip-172-31-80-241.ec2.internal>
2025-07-21 03:07:13 +0000

    Prueba pipeline

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/iot_backend_deploy
+pip install -r requirements.txt
---
+++ scripts/start_application.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/iot_backend_deploy
+nohup python3 app.py > app.log 2>&1 &
+1~#!/bin/bash
+cd /home/ubuntu/iot_backend_deploy
+nohup python3 app.py > app.log 2>&1 &
